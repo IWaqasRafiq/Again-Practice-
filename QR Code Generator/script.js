03e55d8a94ddee35b0c022d1bb271ae092aae7cd
waqas rafiq <wm66179@gmail.com>
2025-06-03 23:56:51 +0500

    Complete QR generator

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -3,7 +3,14 @@ const qrImage = document.getElementById("qrImage");
 const qrText = document.getElementById("qrText");
 
 function generateQr() {
-  qrImage.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrText.value;
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+      imgBox.classList.add("show-img");    
+  }else{
+    setTimeout(() => {
+      qrText.classList.add('error');
+    }, 1000);
+  }
 }
